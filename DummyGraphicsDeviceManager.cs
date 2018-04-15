80ff9d6a8d9f060938c70bf7c58643f56ca02af4
namza666 <namza_tanapon3@outlook.com>
2018-04-15 19:36:29 +0700

    reset

---
+++ DummyGraphicsDeviceManager.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project_game_ver0._3
+{
+    internal class DummyGraphicsDeviceManager : IGraphicsDeviceService
+    {
+        public GraphicsDevice GraphicsDevice { get; private set; }
+
+        // Not used:
+        public event EventHandler<EventArgs> DeviceCreated;
+        public event EventHandler<EventArgs> DeviceDisposing;
+        public event EventHandler<EventArgs> DeviceReset;
+        public event EventHandler<EventArgs> DeviceResetting;
+
+        public DummyGraphicsDeviceManager(GraphicsDevice graphicsDevice)
+        {
+            GraphicsDevice = graphicsDevice;
+        }
+    }
+    
+}
